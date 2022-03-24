60d8eefa748caaa200e361020a9b2f3600989326
djrich87 <djrich87@gmail.com>
2022-03-24 11:57:26 -0600

    remove create function in profile.js

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -9,22 +9,7 @@ function index(req, res) {
   })
 }
 
-function create(req, res) {
-  Profile.findById(req.user.profile._id)
-  .then(profile => {
-    profile.teams.push(req.body)
-    profile.save()
-    .then(() =>{
-      res.redirect(`/profiles/${req.user.profile._id}`)
-    })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect(`/profiles/${req.user.profile._id}`)
-  })
-}
 
 export { 
   index,
-  create,
 }
