3b15a9ae19d04544850f65f79d6eb79ed76e2eca
djrich87 <djrich87@gmail.com>
2022-03-24 10:15:28 -0600

    add create function in profile.js

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -9,4 +9,22 @@ function index(req, res) {
   })
 }
 
-export { index }
+function create(req, res) {
+  Profile.findById(req.user.profile._id)
+  .then(profile => {
+    profile.teams.push(req.body)
+    profile.save()
+    .then(() =>{
+      res.redirect(`/profiles/${req.user.profile._id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
+}
+
+export { 
+  index,
+  create,
+}
